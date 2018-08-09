b8e628cecb25056afba82892a64d15f2fe741a0b
OS X10_12_6 <lf@mifengs.com>
2018-08-09 18:15:29 +0800

    修改

---
+++ Lottery/Lottery.h
@@ -0,0 +1,19 @@
+//
+//  Lottery.h
+//  Lottery
+//
+//  Created by OS X10_12_6 on 2018/8/9.
+//  Copyright © 2018年 OS X10_12_6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lottery.
+FOUNDATION_EXPORT double LotteryVersionNumber;
+
+//! Project version string for Lottery.
+FOUNDATION_EXPORT const unsigned char LotteryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lottery/PublicHeader.h>
+
+
---
+++ LotteryTicket/LotteryTicket.h
@@ -0,0 +1,19 @@
+//
+//  LotteryTicket.h
+//  LotteryTicket
+//
+//  Created by OS X10_12_6 on 2018/8/9.
+//  Copyright © 2018年 OS X10_12_6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LotteryTicket.
+FOUNDATION_EXPORT double LotteryTicketVersionNumber;
+
+//! Project version string for LotteryTicket.
+FOUNDATION_EXPORT const unsigned char LotteryTicketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LotteryTicket/PublicHeader.h>
+
+
